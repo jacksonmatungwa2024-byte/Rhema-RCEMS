eca81f0f398b7f79125a096a3d611f3bac63aaf4
jacksonmatungwa2024-byte <jacksonmatungwa2024@gmail.com>
2025-11-12 23:20:43 +0300

    Delete src/app/UpdatePassword directory

--- src/app/UpdatePassword/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-export const dynamic = "force-dynamic"; // <-- tells Next.js: do not prerender
-
-import UpdatePassword from "./UpdatePassword";
-
-export default function Page() {
-  return <UpdatePassword />;
-}
