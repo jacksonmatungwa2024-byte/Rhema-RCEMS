6b4a521de3558c05c2a151557d66f4f0d22696e8
jacksonmatungwa2024-byte <jacksonmatungwa2024@gmail.com>
2025-11-13 00:23:04 +0300

    Update UserList.tsx

--- src/app/admin/user-management/UserList.tsx
+++ src/app/admin/user-management/UserList.tsx
@@ -1,1 +1,16 @@
+"use client";
+import React from "react";
+import UserCard from "./UserCard";
+
+export default function UserList({ users, refresh }: any) {
+  if (!users.length) return <p>Hakuna watumiaji waliopatikana.</p>;
+
+  return (
+    <div className="user-list">
+      {users.map((user: any) => (
+        <UserCard key={user.id} user={user} refresh={refresh} />
+      ))}
+    </div>
+  );
+}
 
