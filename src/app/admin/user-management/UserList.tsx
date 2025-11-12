7e44bb9eb47f62757d0d7dbe39a4e182132b31f1
jacksonmatungwa2024-byte <jacksonmatungwa2024@gmail.com>
2025-11-13 01:12:19 +0300

    Update UserList.tsx

--- src/app/admin/user-management/UserList.tsx
+++ src/app/admin/user-management/UserList.tsx
@@ -1,16 +1,28 @@
 "use client";
-import React from "react";
+
 import UserCard from "./UserCard";
 
-export default function UserList({ users, refresh }: any) {
-  if (!users.length) return <p>Hakuna watumiaji waliopatikana.</p>;
+interface UserListProps {
+  users: any[];
+  onDelete: (id: number, email: string) => void;
+  onGenerateOtp: (id: number, email: string, countryCode: string) => void;
+  onApprove: (id: number) => void;
+  saving: boolean;
+}
 
+export default function UserList({ users, onDelete, onGenerateOtp, onApprove, saving }: UserListProps) {
   return (
-    <div className="user-list">
-      {users.map((user: any) => (
-        <UserCard key={user.id} user={user} refresh={refresh} />
+    <div>
+      {users.map(user => (
+        <UserCard
+          key={user.id}
+          user={user}
+          onDelete={onDelete}
+          onGenerateOtp={onGenerateOtp}
+          onApprove={onApprove}
+          saving={saving}
+        />
       ))}
     </div>
   );
 }
-
