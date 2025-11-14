34d4f76ccc394539b0a05bf75c10abb04e9e2386
jacksonmatungwa2024-byte <jacksonmatungwa2024@gmail.com>
2025-11-15 00:37:57 +0300

    Update sendWhatsappOtp.ts

--- src/app/admin/user-management/sendWhatsappOtp.ts
+++ src/app/admin/user-management/sendWhatsappOtp.ts
@@ -1,5 +1,5 @@
 export function sendWhatsappOtp(fullNumber: string, otp: string) {
-  const message = `🔐 OTP yako ni: ${otp}. Usipishie mtu mwingine.`;
+  const message = `🔐 OTP yako ni: ${otp}. Usimpatie mtu mwingine, Umalizapo kutumia futa ujumbe kwa ajili ya usalama nakushukru sana nikutakie wakati mwema🙏`;
   const waLink = `https://wa.me/${fullNumber.replace(/\D/g, "")}?text=${encodeURIComponent(message)}`;
   window.open(waLink, "_blank")?.focus();
 }
