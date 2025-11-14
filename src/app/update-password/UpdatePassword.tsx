2ae3a2853aecce3ed46bc31a2e410836756a5a98
jacksonmatungwa2024-byte <jacksonmatungwa2024@gmail.com>
2025-11-15 00:06:12 +0300

    Update UpdatePassword.tsx

--- src/app/update-password/UpdatePassword.tsx
+++ src/app/update-password/UpdatePassword.tsx
@@ -2,7 +2,7 @@
 import { useSearchParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { createClient } from "@supabase/supabase-js";
-
+import "./UpdatePassword.css";
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
