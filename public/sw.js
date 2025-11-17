b4571d12166c56a46e6d1415e44b6ce41a4b93da
jacksonmatungwa2024-byte <jacksonmatungwa2024@gmail.com>
2025-11-17 14:08:28 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,40 +1,26 @@
-const CACHE_NAME = "kanisa-cache-v1";
-const urlsToCache = ["/", "/manifest.json"];
-
-// Install event
 self.addEventListener("install", (event) => {
-  console.log("Service Worker: Installed");
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Service Worker: Caching files");
-      return cache.addAll(urlsToCache);
+    caches.open("lumina-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/offline.html",
+        "/manifest.json",
+        "/aerial.mp4",
+        "/theme.mp3",
+        "/intro-tone.mp3",
+        "/icons/icon-192.png",
+        "/icons/icon-256.png",
+        "/icons/icon-384.png",
+        "/icons/icon-512.png"
+      ]);
     })
   );
 });
 
-// Activate event
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker: Activated");
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log("Service Worker: Clearing old cache");
-            return caches.delete(cache);
-          }
-        })
-      )
-    )
-  );
-});
-
-// Fetch event
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Return cached response if found, else fetch from network
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request).then((res) => {
+      return res || caches.match("/offline.html");
+    }))
   );
 });
