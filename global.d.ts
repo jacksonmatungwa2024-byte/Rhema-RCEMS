b68c5e0842c47029f27fac6c5958536ac538c331
jacksonmatungwa2024-byte <jacksonmatungwa2024@gmail.com>
2025-11-18 16:05:38 +0300

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,11 @@
+// Ruhusu import ya CSS files
 declare module "*.css";
+
+// Ruhusu import ya bcryptjs bila type errors
 declare module "bcryptjs";
+
+// Ruhusu import ya jsonwebtoken bila type errors
 declare module "jsonwebtoken";
+
+// Pia unaweza kuongeza speakeasy hapa
+declare module "speakeasy";
