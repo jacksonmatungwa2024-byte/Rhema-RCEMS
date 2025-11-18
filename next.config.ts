730502e19ddaefa01f76a6b985227d6957ad7fd8
jacksonmatungwa2024-byte <jacksonmatungwa2024@gmail.com>
2025-11-18 21:35:47 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // routes zote
+        headers: [
+          { key: "X-Frame-Options", value: "DENY" }, // zuia iframe embedding
+          { key: "Content-Security-Policy", value: "frame-ancestors 'none'" }, // zuia kutumika kwenye apps zingine
+          { key: "Referrer-Policy", value: "same-origin" }, // ruhusu referrer kutoka domain yako tu
+          { key: "X-Robots-Tag", value: "noindex, nofollow" }, // zuia indexing na search engines
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
