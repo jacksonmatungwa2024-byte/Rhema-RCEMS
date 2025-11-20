257b8da82d6a0bbc6fb4d8bc0edff457c298ccac
jacksonmatungwa2024-byte <jacksonmatungwa2024@gmail.com>
2025-11-20 16:14:20 +0300

    Update route.ts

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: Request) {
 
     if (role === "admin") {
       const secret = speakeasy.generateSecret({
-        name: `Lumina App (${email})`, // jina litaonekana kwenye Google Authenticator
+        name: `Lumina App (Kanisa) (${email})`, // jina litaonekana kwenye Google Authenticator
       });
       totpSecret = secret.base32;
 
